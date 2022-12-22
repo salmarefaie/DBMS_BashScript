575982272c331d20768fefc686730376c45c9869
salmarefaie <salmarefaie29@gmail.com>
2022-12-21 22:48:54 -0500

    regex

--- Create_DataBase.sh
+++ Create_DataBase.sh
@@ -4,11 +4,20 @@ echo " -------------------"
 echo "| Database Creation |"
 echo " -------------------"
 read -p "Enter your database name: " databaseName
+
+
+while [[ -z $databaseName || $databaseName == [0-9]* || $databaseName == *['!''@#/$\"*{^(+/|,;:~`.%&.=-]>[<?']* ]] 
+do
+   read -p "Inavalid Databse Name, enter another name: " databaseName
+done
+
+
 while [ -e $databaseName ]
 do
    read -p "Databse name already exist, enter another name: " databaseName
 done
-mkdir $databaseName
+  
+mkdir $databaseName 
 echo " --------------------- "
 echo "| Database is created |"
 echo " --------------------- "
