1abde7e0711004833cfafe9947f9cecb4c6c54cc
Alaa Dwidar <alaaismail2088@gmail.com>
2022-12-27 12:33:04 +0200

    insert

--- Select_Colomn.sh
+++ Select_Colomn.sh
@@ -21,3 +21,37 @@ numSelectColumns=$(head -1 ./$selectTable"_metadata" | awk -F : '
 
 # take id or name or ....
 read -p "Enter name column which is selected row by it: " column
+
+# check regex column name
+if [[ -z $column || $column == [0-9]* || $column == *['!''@#/$\"*{^(+/|,;:~`.%&.=-]>[<?']* || $column == *" "* ]] ;then
+   echo " ---------------------- "
+   echo "| Inavalid Column Name |" 
+   echo " ---------------------- "
+
+declare -i index         # num of column for table
+
+# check lw mwgood fy table metadata
+elif [[ ${col_selectArr[*]} =~ $column ]] ; then
+
+      # bageeb num of column for table
+      for (( i=1; i <= $numSelectColumns ; i++ ))
+      do 
+         if [[ "${col_selectArr[i-1]}" == "$column" ]] ; then
+            index=$i
+         fi
+      done
+      
+      # select colomn values
+      echo " ------ "
+      echo "| ${col_selectArr[index-1]} |"
+      echo " ------ "
+      cat ./$selectTable | cut -d":" -f $index
+      
+# column name m4 mwgood
+else
+   echo " ---------------------- "
+   echo "| Column doesn't Exist |"
+   echo " ---------------------- "
+
+fi
+break
